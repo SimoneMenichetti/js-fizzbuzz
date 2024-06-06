6529a70928133606885202cc5a84ab26d0e04814
simone menichetti <simon3menichettui95@gmail.com>
2024-06-06 19:39:56 +0200

    inseriti i div.style.backgroundColor per modificare il colore dei quadrati a seconda del valore + formattazione codice

--- Bonusstyle/js/mainjavascript.js
+++ Bonusstyle/js/mainjavascript.js
@@ -8,7 +8,7 @@ for (let i = 1; i <=100; i++) {
 
     // utilizzo console log per verifica stamp numeri da 1 a 100
     console.log(i);
-// utilizzando un ciclo con for se è un multiplo di 3 sarà "Fiz" di 5 sarà "Buzz" o di entrambi 3-5 "FizBuzz"
+    // utilizzando un ciclo con for se è un multiplo di 3 sarà "Fiz" di 5 sarà "Buzz" o di entrambi 3-5 "FizBuzz"
 
     if (i % 3 === 0 && i % 5 === 0) {
         output = "FizzBuzz";
@@ -23,6 +23,9 @@ for (let i = 1; i <=100; i++) {
         output = i;
     }
 
+    // inseriamo console log output per verificare in console le variabili dei valori
+    console.log(output);
+
     // creiamo un nuovo elemento div
     const div = document.createElement('div');
 
@@ -31,27 +34,32 @@ for (let i = 1; i <=100; i++) {
         // per il testo
     div.textContent = output;
         // aggiunta classe
-    div.classList.add(".quadrato");
+    div.classList.add("quadrato", output);
 
 
-       // Aggiungo classi specifiche per i diversi casi di style
+    // aggiungo i colori di bg style a seconda del valore
       
     if (output === "Fizz") {
-           div.classList.add("fizz");
+        div.classList.add("fizz");
+        div.style.backgroundColor = "#0CD6A1"; 
 
     } else if (output === "Buzz") {
-           div.classList.add("buzz");
+        div.classList.add("buzz");
+        div.style.backgroundColor = "#FFD166"; 
 
     } else if (output === "FizzBuzz") {
-           div.classList.add("fizzbuzz");
+        div.classList.add("fizzbuzz");
+        div.style.backgroundColor = "#F0466F"; 
 
     } else {
         div.classList.add("numero");
+        div.style.backgroundColor =  "#1389B2"; 
     }
 
-    // appendere l'elemento al contenitore principale utilizzando append()
-    contenitorePrincipaleNelMain.append(div);
-   
+
+        // appendere l'elemento al contenitore principale utilizzando append()
+        contenitorePrincipaleNelMain.append(div);
+    
        
   
 }
