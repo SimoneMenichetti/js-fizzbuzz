0d6771d9d4265b98e5ceca00baef23020a2b43b7
simone menichetti <simon3menichettui95@gmail.com>
2024-06-06 17:41:49 +0200

    inserite regole css + inserito ciclo per le variabili di multipli da 3 a 5

--- js/mainjavascript.js
+++ js/mainjavascript.js
@@ -13,9 +13,18 @@ for (let i = 1; i <=100; i++) {
 
     const element = `<div class="quadrato ${i}">${i}</div>`;
     contenitorePrincipaleNelMain.innerHTML += element;
+    
+// utilizzando un ciclo con for se è un multiplo di 3 sarà "Fiz" di 5 sarà "Buzz" o di entrambi 3-5 "FizBuzz"
 
-
-
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
 
 }
 
