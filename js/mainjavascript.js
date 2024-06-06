50cd922b4b167073abe8a517fb0281ef52c81d71
simone menichetti <simon3menichettui95@gmail.com>
2024-06-06 15:32:54 +0200

    inizio applicando il ciclo per stampare in console i numeri da x a y ed utilizzo successivamente il console log per verificare il corretto funzionamento in console

---
+++ js/mainjavascript.js
@@ -0,0 +1,9 @@
+// applico il ciclo per stampare in console i numeri da 1 a 100
+
+for (let i = 1; i <=100; i++) {
+
+    // utilizzo console log per verifica stamp numeri da 1 a 100
+    console.log(i);
+
+}
+
