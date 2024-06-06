0ff334695587b049670e7bba6bd7cb63f7d9b0d7
simone menichetti <simon3menichettui95@gmail.com>
2024-06-06 18:12:44 +0200

    inizio bonus 1 append()

---
+++ Bonus1/js/mainjavascript.js
@@ -0,0 +1,30 @@
+// creazione costante per la selezione di un elemento in dom
+
+const contenitorePrincipaleNelMain = document.querySelector(".container");
+
+// applico il ciclo per stampare in console i numeri da 1 a 100
+
+for (let i = 1; i <=100; i++) {
+
+    // utilizzo console log per verifica stamp numeri da 1 a 100
+    console.log(i);
+// utilizzando un ciclo con for se è un multiplo di 3 sarà "Fiz" di 5 sarà "Buzz" o di entrambi 3-5 "FizBuzz"
+
+    let output = "";
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        output = "FizzBuzz";
+    } else if (i % 3 === 0) {
+        output = "Fizz";
+    } else if (i % 5 === 0) {
+        output = "Buzz";
+    } else {
+        output = i;
+    }
+
+    const element = `<div class="quadrato ${output}">${output}</div>`;
+    // inseriamo la console per verificare gli output 
+    console.log(output)
+    // stampiamo nel dom le value
+    contenitorePrincipaleNelMain.innerHTML += element;
+}
