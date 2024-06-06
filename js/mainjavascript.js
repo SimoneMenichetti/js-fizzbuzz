6058c98a65dbf08852ca1497e988bb048a6138e6
simone menichetti <simon3menichettui95@gmail.com>
2024-06-06 16:04:23 +0200

    creazione di una costante per la selezione di un element nel dom ed inserisco nel dom lo stamp dei numeri inserendoli in piu elementi div

--- js/mainjavascript.js
+++ js/mainjavascript.js
@@ -1,9 +1,21 @@
+// creazione costante per la selezione di un elemento in dom
+
+const contenitorePrincipaleNelMain = document.querySelector(".container");
+
 // applico il ciclo per stampare in console i numeri da 1 a 100
 
 for (let i = 1; i <=100; i++) {
 
     // utilizzo console log per verifica stamp numeri da 1 a 100
     console.log(i);
+
+    // inserisco nel dom lo stamp dei numeri da 1 a 0 inserendoli in piu elementi div
+
+    const element = `<div class="quadrato ${i}">${i}</div>`;
+    contenitorePrincipaleNelMain.innerHTML += element;
+
+
+
 
 }
 
